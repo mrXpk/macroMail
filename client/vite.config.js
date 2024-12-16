087224d79bc7f17d33070155c0ffa37758409656
mrXpk <pkdotss@gmail.com>
2024-12-16 22:11:36 +0545

    Initial commit: MacroMail - Disposable Email Service

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/macroMail/',  // Add this line - should match your repository name
   server: {
     port: 5173,
     host: true,
